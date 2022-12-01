e37a12ed5741c9d0f20a6a330ec633efd77b33be
Paul Rosset <paul.rosset@qonto.com>
2022-12-01 12:03:12 +0100

    Week 1st Dec

--- app/welcome/route.js
+++ app/welcome/route.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class WelcomeRoute extends Route {}
+export default class WelcomeRoute extends Route {
+  @service store;
+
+  model() {
+    return this.store.findRecord('todo', 1);
+  }
+}
